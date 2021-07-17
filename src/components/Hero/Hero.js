9a895e2becd4ac2c14ff48155410a28d5b401168
TanyaradzwaGozhora <51749063+TanyaradzwaGozhora@users.noreply.github.com>
2021-07-17 14:59:23 +0200

    Updates

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,19 @@ import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
   <div>
-    Hero
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome to <br/>
+          my portfolio(to be replaced)
+        </SectionTitle>
+        <SectionText>
+          # my bio (I'm dadada)
+        </SectionText>
+        #If someone wants to hire you or check out more of your work 
+        <Button onClick={()=>window.location="#about"}>Learn More</Button>
+      </LeftSection>
+    </Section>
   </div>
 );
 
