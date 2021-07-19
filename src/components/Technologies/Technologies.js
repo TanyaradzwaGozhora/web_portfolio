fbc657d5a5073ce7b6c431fc01ce77a4c6e74240
TanyaradzwaGozhora <51749063+TanyaradzwaGozhora@users.noreply.github.com>
2021-07-19 22:24:00 +0200

    Updates Tech section added

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact } from 'react-icons/di';
+import { AiOutlinePartition } from 'react-icons/ai';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies.
+      From Back-end to Data processing and analysis to Front-end and Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js, Next.js, Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node.js, MongoDB, mySQL, Unittest
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <AiOutlinePartition size="3rem"/>
+        <ListContainer>
+          <ListTitle>Machine Learning</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Pandas, Pytorch, AWS Sagemaker
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
