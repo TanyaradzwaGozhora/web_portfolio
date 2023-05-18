c1c9d59013e16e071f8d9e66be68cc6417038f66
TanyaradzwaGozhora <51749063+TanyaradzwaGozhora@users.noreply.github.com>
2023-05-17 23:47:18 -0600

    JUJU

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,8 +19,11 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>An <em>attempted</em> commit a day keeps the boredom away :)<br></br>
+          <Slogan>Small wins ~ Julius Nanai<br></br>
+          {/* <Slogan>An <em>attempted</em> commit a day keeps the boredom away :)<br></br> */}
+
           <br></br>
+          
           
           Strongly inspired by <a  href="https://github.com/adrianhajdin/portfolio_website"> 
           this repo</a>
